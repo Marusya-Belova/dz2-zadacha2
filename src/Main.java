9420ac6246ffcce20ceb4e5d5d2251345967e43c
Marusya-Belova <m9175797050@gmail.com>
2021-11-29 13:30:35 +0300

    Add Files

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int refill = 1100;
+        int bonus;
+        if (refill <= 100) {
+            bonus = 0;
+            System.out.println("Bonus = 0");
+        } else {
+            bonus = refill/100;
+            System.out.print("Bonus = ");
+            System.out.println(bonus);
+        }
+        int resultbalance=balance+refill+bonus;
+        System.out.print("Result Balance = ");
+        System.out.println(resultbalance);
+    }
+}
